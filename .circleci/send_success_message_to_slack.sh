53ba572fb2d142638a50ecf79ab90157db59ece0
kazuhito-m <kazuhito.sumpic@gmail.com>
2018-10-29 19:17:54 +0900

    実装。ソースのチェックアウト後に移動。

--- .circleci/send_success_message_to_slack.sh
+++ .circleci/send_success_message_to_slack.sh
@@ -28,7 +28,6 @@ send_json=$(cat << EOS
   "attachments": [
     {
       "color": "#36a64f",
-      "image_url": "https://previews.dropbox.com/p/thumb/AANP03t-WAHTTCAcoKgNGMxj7XEsAM-GHWm7jms_Zf1thutO23U61mY1xfFAv69xONCA00K4-Uuw12TVs64nCaQs6_suVDoaPT3TWoDerajJ3K5cGxHyR19JJJJ_U31cdUmy085BKhSNpWB8M27zfImiEzYadMz_8Ix4rtJ_vrH29EQUcGGp7mzpH4bv_iKtxE0d-GEmoOyGV1iRjVXe-TsSCsH-UU8CgS70jIY2IO_MCA/p.png?size=1600x1200&size_mode=3",
       "fields": [
         {
           "title": "${title}",
